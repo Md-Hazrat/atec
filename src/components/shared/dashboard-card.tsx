b25f4ee6db2ef54cef8a56d3d7f8723b4db7e4e8
Md-Hazrat <mdhazratali8866@gmail.com>
2024-05-31 22:58:03 +0600

    working on dashboard

--- src/components/shared/dashboard-card.tsx
+++ src/components/shared/dashboard-card.tsx
@@ -16,13 +16,15 @@ const DashboardCard = ({
 }: DashboardCardProps) => {
   return (
     <>
-      <div className="container rounded-lg shadow-md dark:border">
-        <p className="flex flex-row-reverse mt-4">{icon}</p>
-        <div className="pb-4">
+    <div className="container rounded-lg shadow-md dark:border p-4">
+     <div className="flex justify-between"> 
           <h3 className="text-lg font-semibold">{title}</h3>
-          <p className="text-2xl font-bold">{price}</p>
-          <p className="text-sm text-green-500">{description}</p>
+          <p className="mt-2">{icon}</p>
         </div>
+      <div className="">
+        <p className="text-2xl font-bold">{price}</p>
+        <p className="text-sm text-green-500">{description}</p>
+      </div>
       </div>
     </>
   );
